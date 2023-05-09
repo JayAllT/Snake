939bd647d9cd9ba37a0d65f8d57c3e7861c017dc
unknown <jayalltul@gmail.com>
2023-05-09 23:22:23 +1000

    commit

---
+++ Main.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+public class Main : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    public static Random rnd = new Random();
+
+    Texture2D appleText;
+    Texture2D bgText;
+    Texture2D snakeText;
+
+    // snake
+    Snake snake = new Snake();
+
+    // vectors
+    Vector2 zero = new Vector2(0, 0);
+
+    public Main()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        _graphics.PreferredBackBufferWidth = 800;
+        _graphics.PreferredBackBufferHeight = 600;
+        _graphics.ApplyChanges();
+
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        // load textures
+        appleText = Content.Load<Texture2D>("apple");
+        bgText = Content.Load<Texture2D>("bg");
+        snakeText = Content.Load<Texture2D>("snake");
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        // draw textures
+        _spriteBatch.Begin();
+
+        _spriteBatch.Draw(bgText, zero, Color.White);
+
+        // draw snake
+        for (int i = 0; i < snake.length; i++)
+        {
+            _spriteBatch.Draw(snakeText, snake.position[i] * 25, Color.White);
+        }
+
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
---
+++ Snake.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+internal class Snake
+{
+    // keeps track of places snake is, in order
+    public List<Vector2> position = new List<Vector2>();
+    public Vector2 headPos = new Vector2();
+
+    public int length = 1;
+
+    // 0 = right, 1 = down, 2 = left, 3 = up
+    public int dir = 0;
+
+    public Snake()
+    {
+        // snake spawn pos
+        headPos = new Vector2(Main.rnd.Next() % 33, Main.rnd.Next() % 24);
+        position.Add(headPos);
+    }
+}
